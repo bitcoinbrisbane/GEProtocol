a1c891f86c74903e7fdb6fefec2aacbbfd180536
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-08 15:09:18 +1000

    chore: remove project

--- hh/ignition/modules/UniswapV3FactoryModule.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("UniswapV3Factory", (m) => {
-  const factory = m.contract("UniswapV3Factory");
-
-  return { factory };
-});
--- hh/ignition/modules/UniswapV3PoolDeployerModule.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("UniswapV3PoolDeployerModule", (m) => {
-  const deployer = m.contract("UniswapV3PoolDeployer");
-
-  return { deployer };
-});
