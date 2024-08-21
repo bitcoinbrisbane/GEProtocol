3925673821790e143238509689ee00c469dada7a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-21 16:40:24 +1000

    chore: Add UniswapV3Factory address to deployed_addresses.json and update createpool.js

--- blockchain/v3-core/scripts/createpool.js
+++ blockchain/v3-core/scripts/createpool.js
@@ -1,12 +1,19 @@
-const ethers = require('ethers');
 
-const poolFactory = require('../artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json');
+const { ethers, JsonRpcProvider } = require('ethers');
 
-const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+const poolFactory = require('../artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json')
 
-const wallet = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider);
+const provider = new JsonRpcProvider('http://localhost:8545')
 
-const factory = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', poolFactory.abi, wallet);
+const wallet = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider)
+const factory = new ethers.Contract('0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', poolFactory.abi, wallet)
 
-const tokenA = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const tokenB = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const tokenA = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+const tokenB = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+
+const deploy = async () => {
+  const pool = await factory.createPool(tokenA, tokenB, 3000)
+  console.log(pool)
+}
+
+deploy()
