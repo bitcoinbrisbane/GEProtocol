2da2a59117b102bea1826017a485288fe29d1dc5
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-28 17:24:11 +1000

    feat: Add v2getprice.js script for fetching prices from Uniswap V2

--- blockchain/uniswap-v2-core/scripts/v2getprice.js
+++ blockchain/uniswap-v2-core/scripts/v2getprice.js
@@ -3,12 +3,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { ethers, JsonRpcProvider } = require('ethers')
 
-const factory_json = require('../artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json')
-
 const provider = new JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/0PISwk2ZJQ4XJt4-OHdq5ACx_jmnGzxX')
 
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
-const factory = new ethers.Contract('0xF6E9ff447C8E7bb2098eb558557C34B483ec928E', factory_json.abi, wallet)
+const factory = new ethers.Contract('0x22d06d680aBfE8638a2F2136656325Af518C3FD8', factory_json.abi, wallet)
+const pair = new ethers.Contract('0x805C24890478BbCd82c8Ce2cF73d9cb00cC7715C', factory_json.abi, wallet)
 
 const tokenA = '0x574084E6A21cD334277B79f35F98C0Aae24E0030'
 const tokenB = '0x945d9Eb083b477472870B2D1a3FAbaFc7ad70F00'
@@ -24,4 +23,3 @@ const getPool = async () => {
 }
 
 // createPool()
-getPool()
