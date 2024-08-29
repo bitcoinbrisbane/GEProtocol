4b69497224d69929dedd18151b347c10a953918d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-29 14:54:55 +1000

    chore: Update UniswapV2 dependencies and add v2getprice.js script

--- blockchain/uniswap-v2-core/scripts/v2getprice.js
+++ blockchain/uniswap-v2-core/scripts/v2getprice.js
@@ -3,23 +3,75 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { ethers, JsonRpcProvider } = require('ethers')
 
-const provider = new JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/0PISwk2ZJQ4XJt4-OHdq5ACx_jmnGzxX')
+const provider = new JsonRpcProvider(process.env.HTTPS_PROVIDER)
 
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
-const factory = new ethers.Contract('0x22d06d680aBfE8638a2F2136656325Af518C3FD8', factory_json.abi, wallet)
-const pair = new ethers.Contract('0x805C24890478BbCd82c8Ce2cF73d9cb00cC7715C', factory_json.abi, wallet)
+const get_pair_abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "tokenA",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "tokenB",
+        "type": "address"
+      }
+    ],
+    "name": "getPair",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+
+const get_reserves_abi = [
+  {
+    "inputs": [],
+    "name": "getReserves",
+    "outputs": [
+      {
+        "internalType": "uint112",
+        "name": "_reserve0",
+        "type": "uint112"
+      },
+      {
+        "internalType": "uint112",
+        "name": "_reserve1",
+        "type": "uint112"
+      },
+      {
+        "internalType": "uint32",
+        "name": "_blockTimestampLast",
+        "type": "uint32"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+
+// const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+const factory = new ethers.Contract('0x22d06d680aBfE8638a2F2136656325Af518C3FD8', get_pair_abi, provider)
+const pair = new ethers.Contract('0x805C24890478BbCd82c8Ce2cF73d9cb00cC7715C', get_pair_abi, provider)
 
 const tokenA = '0x574084E6A21cD334277B79f35F98C0Aae24E0030'
 const tokenB = '0x945d9Eb083b477472870B2D1a3FAbaFc7ad70F00'
 
-const createPool = async () => {
-  const pool = await factory.createPool(tokenA, tokenB, 3000)
-  console.log(pool)
+const getPrice = async () => {
+  const pair = await factory.getPair(tokenA, tokenB)
+  console.log(pair)
+
+  const pairContract = new ethers.Contract(pair, get_reserves_abi, provider)
+  const reserves = await pairContract.getReserves()
+  console.log(reserves)
 }
 
-const getPool = async () => {
-  const pool = await factory.getPool(tokenA, tokenB, 3000)
-  console.log(pool)
-}
-
-// createPool()
+getPrice()
